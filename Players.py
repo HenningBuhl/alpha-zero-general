1f296191a6ca14106c67a6e9db4049527a26cf40
HenningBuhl <henning.buhl@yahoo.de>
2020-01-01 04:05:46 +0000

    Added Players.py with generic players for all games.

---
+++ Players.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+"""
+Random and Human-ineracting players for the game of TicTacToe.
+
+Author: Evgeny Tyurin, github.com/evg-tyurin
+Date: Jan 5, 2018.
+
+Based on the OthelloPlayers by Surag Nair.
+
+"""
+class RandomPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        a = np.random.randint(self.game.getActionSize())
+        valids = self.game.getValidMoves(board, 1)
+        while valids[a]!=1:
+            a = np.random.randint(self.game.getActionSize())
+        return a
+
+
+# TODO: Make code generic for any game.
+class HumanPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        # display(board)
+        valid = self.game.getValidMoves(board, 1)
+        for i in range(len(valid)):
+            if valid[i]:
+                print(int(i/self.game.n), int(i%self.game.n))
+        while True:
+            # Python 3.x
+            a = input()
+            # Python 2.x 
+            # a = raw_input()
+
+            x,y = [int(x) for x in a.split(' ')]
+            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
+            if valid[a]:
+                break
+            else:
+                print('Invalid')
+
+        return a
+
+
+class OneStepLookaheadPlayer():
+    """Simple player who always takes a win if presented, or blocks a loss if obvious, otherwise is random."""
+    def __init__(self, game, verbose=True):
+        self.game = game
+        self.player_num = 1
+        self.verbose = verbose
+
+    def play(self, board):
+        valid_moves = self.game.getValidMoves(board, self.player_num)
+        win_move_set = set()
+        fallback_move_set = set()
+        stop_loss_move_set = set()
+        for move, valid in enumerate(valid_moves):
+            if not valid: continue
+            if -self.player_num == self.game.getGameEnded(*self.game.getNextState(board, self.player_num, move)):
+                win_move_set.add(move)
+            elif -self.player_num == self.game.getGameEnded(*self.game.getNextState(board, -self.player_num, move)):
+                stop_loss_move_set.add(move)
+            else:
+                fallback_move_set.add(move)
+
+        if len(win_move_set) > 0:
+            ret_move = np.random.choice(list(win_move_set))
+            if self.verbose: print('Playing winning action %s from %s' % (ret_move, win_move_set))
+        elif len(stop_loss_move_set) > 0:
+            ret_move = np.random.choice(list(stop_loss_move_set))
+            if self.verbose: print('Playing loss stopping action %s from %s' % (ret_move, stop_loss_move_set))
+        elif len(fallback_move_set) > 0:
+            ret_move = np.random.choice(list(fallback_move_set))
+            if self.verbose: print('Playing random action %s from %s' % (ret_move, fallback_move_set))
+        else:
+            raise Exception('No valid moves remaining: %s' % game.stringRepresentation(board))
+
+        return ret_move
