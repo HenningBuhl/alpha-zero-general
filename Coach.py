4d93ad15f9517d0fe7c20a83f1a4b970a650d96d
Henning Buhl <henning.buhl@yahoo.de>
2020-02-07 21:22:10 +0100

    Removed print statement
    When trainExamplesHistory is full and older examples are removed, there will no longer be a message printed.

--- Coach.py
+++ Coach.py
@@ -101,7 +101,7 @@ class Coach():
                 self.trainExamplesHistory.append(iterationTrainExamples)
                 
             if self.args.numItersForTrainExamplesHistory is not None and len(self.trainExamplesHistory) > self.args.numItersForTrainExamplesHistory:
-                print("len(trainExamplesHistory) =", len(self.trainExamplesHistory), " => remove the oldest trainExamples")
+                #print("len(trainExamplesHistory) =", len(self.trainExamplesHistory), " => remove the oldest trainExamples")
                 self.trainExamplesHistory.pop(0)
             # backup history to a file
             # NB! the examples were collected using the model from the previous iteration, so (i-1)  
