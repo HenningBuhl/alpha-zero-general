3e0215c0c9a93fd9e7d6f9333477f44001baeca4
Henning Buhl <henning.buhl@yahoo.de>
2020-02-18 21:03:08 +0100

    Added name, verbose, talk and reset

--- Players.py
+++ Players.py
@@ -1,79 +1,135 @@
 import numpy as np
 from utils import *
 
-"""
-Random and Human-ineracting players for the game of TicTacToe.
-
-Author: Evgeny Tyurin, github.com/evg-tyurin
-Date: Jan 5, 2018.
-
-Based on the OthelloPlayers by Surag Nair.
-
-"""
 
 class Player():
-    def __init__(self, game, args=None, verbose=False):
+    '''
+    This class represents a Player competing in a game.
+    '''
+    def __init__(self, game, args=None, name=None, verbose=False):
+        '''
+        Args:
+            game: The game to play.
+            args: The arguments (default None).
+            name: The name of the player (default None). Is set to class name if None.
+            verbose: The verbosity (default False)
+        
+        Returns:
+            A player object.
+        '''
         self.game = game
         self.args = args
         self.verbose = verbose
+        self.name = name
+        if self.name is None:
+            self.name = type(self).__name__ # Default name equals class name.
 
-    def play(self, board, verbose=False):
+    def reset():
+        '''
+        Reset any data that is created/expanded while playing.
+        '''
         pass
-    
+
+    def play(self, board, curPlayer, verbose=False):
+        '''
+        Args:
+            board: The current board.
+            curPlayer: The current player.
+            verbose: Whether to output information about the decision process (default False).
+        
+        Returns:
+            A tuple of (best_action, expected_outcome, resign).
+        '''
+        pass
+
     def get_verbose(self, verbose=None):
+        '''
+        Args:
+            verbose: (default None).
+        
+        Returns:
+            The verbosity level.
+        '''
         return self.verbose if verbose is None else verbose
 
-class RandomPlayer(Player):
-    def __init__(self, game, args=None, verbose=False):
-        super(RandomPlayer, self).__init__(game, args, verbose)
+    def talk(self, a, v, r, verbose=True):
+        '''
+        Prints various information regarding the decision process of the player.
 
-    def play(self, board, verbose=None):
-        verbose = super(RandomPlayer, self).get_verbose(verbose)
+        Args:
+            a: The action performed.
+            v: The expected outcome of the game.
+            r: Whether the player resigned the game.
+            verbose: Whether to print or not (default True).
+        '''
+        if verbose:
+            if v is not None:
+                # Print expected outcome.
+                print(f'{self.name}\'s outcome prediction: {v}')
+            if r:
+                # Print whether the player resigned the game.
+                print(f'{self.name} resigned the game.')
+            else:
+                # Print which action the player chose.
+                print(f'{self.name} chose action {a}')
+
+
+class RandomPlayer(Player):
+    def __init__(self, game, args=None, name=None, verbose=False):
+        super(RandomPlayer, self).__init__(game, args=args, name=name, verbose=verbose)
+
+    def play(self, board, curPlayer, verbose=None):
+        verbose = get_verbose(verbose=verbose)
         valids = self.game.getValidMoves(board, 1)
         valid_indices = [i for i, valid in enumerate(valids) if valid]
-        a = np.random.choice(valid_indices)
-        if verbose:
-            print(f'Playing random available move: {a}')
-        return a
+        a = np.random.choice(valid_indices) # Chose random action.
+        self.talk(a, None, False, verbose=verbose)
+        return a, None, False
 
 
 class HumanPlayer(Player):
-    def __init__(self, game, args=None, verbose=False):
-        super(HumanPlayer, self).__init__(game, args, verbose)
+    def __init__(self, game, args=None, name=None, verbose=False):
+        super(HumanPlayer, self).__init__(game, args=args, name=name, verbose=verbose)
 
-    def play(self, board, verbose=None):
-        verbose = super(HumanPlayer, self).get_verbose(verbose)
+    def play(self, board, curPlayer, verbose=None):
+        verbose = get_verbose(verbose=verbose)
         valid_moves = self.game.getValidMoves(board, 1)
         uf_moves = self.game.getUserFriendlyMoves(board, 1)
-        for i, (valid, uf_move) in enumerate(uf_moves):
+        print('Pass -1 into action prompt to resign.')
+        for i, (valid, uf_move) in enumerate(uf_moves): # Display available actions.
             if valid:
                 if self.verbose:
                     print(f'Move: {i:2d}, UF: {uf_move}')
 
-        while True:
-            move = int(input())
-            if valid_moves[move]:
+        while True: # Don't continue until user input is fetched.
+            a = int(input())
+            if a == -1: # HumanPlayers wants to resign
                 break
-            else:
-                if verbose:
-                    print('Invalid move')
-        if verbose:
-            print(f'Playing action {move}')
-        return move
+            else: # HumanPlayer wants to play an action.
+                if valid_moves[move]:
+                    break
+                else:
+                    if verbose:
+                        print('Invalid move')
+
+        resign = a == -1 # HumanPlayer can always resign.
+        self.talk(a, None, resign, verbose=verbose)
+        return a, None, resign
 
 
-class OneStepLookaheadPlayer(Player):
+class GreedyPlayer(Player):
     """Simple player who always takes a win if presented, or blocks a loss if obvious, otherwise is random."""
-    def __init__(self, game, args=None, verbose=False):
-        super(OneStepLookaheadPlayer, self).__init__(game, args, verbose)
+    def __init__(self, game, args=None, name=None, verbose=False):
+        super(GreedyPlayer, self).__init__(game, args=args, name=name, verbose=verbose)
         self.player_num = 1
 
-    def play(self, board, verbose=None):
-        verbose = super(OneStepLookaheadPlayer, self).get_verbose(verbose)
+    def play(self, board, curPlayer, verbose=None):
+        verbose = get_verbose(verbose=verbose)
         valid_moves = self.game.getValidMoves(board, self.player_num)
-        win_move_set = set()
-        fallback_move_set = set()
-        stop_loss_move_set = set()
+        win_move_set = set() # Actions that result in a win.
+        stop_loss_move_set = set() # Actions that block a loss.
+        fallback_move_set = set() # All other actions (random).
+        
         for move, valid in enumerate(valid_moves):
             if not valid: continue
             if -self.player_num == self.game.getGameEnded(*self.game.getNextState(board, self.player_num, move)):
@@ -84,37 +140,42 @@ class OneStepLookaheadPlayer(Player):
                 fallback_move_set.add(move)
 
         if len(win_move_set) > 0:
-            ret_move = np.random.choice(list(win_move_set))
-            if verbose:
-                print('Playing winning action %s from %s' % (ret_move, win_move_set))
+            a = np.random.choice(list(win_move_set))
         elif len(stop_loss_move_set) > 0:
-            ret_move = np.random.choice(list(stop_loss_move_set))
-            if verbose:
-                print('Playing loss stopping action %s from %s' % (ret_move, stop_loss_move_set))
+            a = np.random.choice(list(stop_loss_move_set))
         elif len(fallback_move_set) > 0:
-            ret_move = np.random.choice(list(fallback_move_set))
-            if verbose:
-                print('Playing random action %s from %s' % (ret_move, fallback_move_set))
+            a = np.random.choice(list(fallback_move_set))
         else:
-            raise Exception('No valid moves remaining: %s' % game.stringRepresentation(board))
-        return ret_move
+            raise Exception(f'No valid moves remaining: {game.stringRepresentation(board)}')
+        
+        self.talk(a, None, False, verbose=verbose)
+        return a, None, False
 
 
 class AlphaPlayer(Player):
-    def __init__(self, game, nnet, MCTS, args, verbose=False):
-        super(AlphaPlayer, self).__init__(game, args, verbose)
+    def __init__(self, game, nnet, MCTS, args, name=None, verbose=False):
+        super(AlphaPlayer, self).__init__(game, args=args, name=name, verbose=verbose)
         self.nnet = nnet
         self.MCTS = MCTS
+        self.reset()
+
+    def reset(self):
         if self.MCTS is not None:
             self.mcts = self.MCTS(self.game, self.nnet, self.args)
 
-    def play(self, board, verbose=None):
-        verbose = super(AlphaPlayer, self).get_verbose(verbose)
-        if self.MCTS is not None: # Use MCTS.
-            a = np.argmax(self.mcts.getActionProb(board, temp=0))
-        else: # Use vanilla network without MCTS.
-            valids = self.game.getValidMoves(board, 1)
+    def play(self, board, curPlayer, return_pi=False, temp=0, verbose=None):
+        verbose = get_verbose(verbose=verbose)
+        if self.MCTS is not None: # AlphaPlayer or MCTSPlayer.
+            pi, v = self.mcts.getActionProb(board, temp=temp)
+            a = np.argmax(pi)
+            if self.nnet is not None: # AlphaPlayer.
+                # Save time and don't calculate v is resignation is disabled?
+                _, v = self.nnet.predict(board)
+                v = v[0] # Convert [v] into v.
+        else: # RawAlphaPlayer
             pi, v = self.nnet.predict(board)
+            v = v[0] # Convert [v] into v.
+            valids = self.game.getValidMoves(board, 1)
             pi *= valids
             sum_pi = np.sum(pi)
             if sum_pi > 0:
@@ -122,35 +183,50 @@ class AlphaPlayer(Player):
             else: # All moves all equally likely.
                 pi += valids
                 pi /= np.sum(pi)
-                #a = np.random.choice(len(pi), p=pi) # Get random action.
-                #return a
+                #a = np.random.choice(len(pi), p=pi) # Get random action. # RawNet should be deterministic.
             a = np.argmax(pi)
-        if verbose:
-            print(f'Choosing action {a}')
-        return a
+        
+        resign = False
+        if self.args.resignThreshold is not None:
+            resign = v <= self.args.resignThreshold
+
+        v *= curPlayer # Convert cannonical v into absolute v.
+        self.talk(a, v, resign, verbose=verbose)
+        
+        if return_pi:
+            return a, v, resign, pi
+        else:
+            return a, v, resign
 
 
 class RawAlphaPlayer(AlphaPlayer):
-    def __init__(self, game, nnet, args=None, verbose=False):
-        super(RawAlphaPlayer, self).__init__(game, nnet, None, args, verbose)
+    def __init__(self, game, nnet, args=None, name=None, verbose=False):
+        super(RawAlphaPlayer, self).__init__(game, nnet, None, args=args, name=name, verbose=verbose)
 
 
 class MCTSPlayer(AlphaPlayer):
-    def __init__(self, game, MCTS, args, verbose=False):
-        super(MCTSPlayer, self).__init__(game, None, MCTS, args, verbose)
+    def __init__(self, game, MCTS, args, name=None, verbose=False):
+        super(MCTSPlayer, self).__init__(game, None, MCTS, args=args, name=name, verbose=verbose)
 
 
-class AlphaBetaPlayer(Player):
-    def __init__(self, game, ABS, args, verbose=False):
-        super(AlphaBetaPlayer, self).__init__(game, args, verbose)
+class ABTSPlayer(Player):
+    def __init__(self, game, ABTS, args, name=None, verbose=False):
+        super(ABTSPlayer, self).__init__(game, args=args, name=name, verbose=verbose)
         self.game = game
-        self.ABS = ABS
-        self.abs = self.ABS(self.game, self.args)
+        self.ABTS = ABTS
+        self.reset()
 
-    def play(self, board, verbose=None):
-        verbose = super(AlphaBetaPlayer, self).get_verbose(verbose)
-        a = np.argmax(self.abs.getActionProb(board))
-        if verbose:
-            print(f'Choosing action {a}')        
-        return a
+    def reset(self):
+        self.abts = self.ABTS(self.game, self.args)
+
+    def play(self, board, curPlayer, verbose=None):
+        verbose = get_verbose(verbose=verbose)
+        probs, v = self.abts.getActionProb(board)
+        a = np.argmax(probs)
+        resign = False
+        if self.args.resignThreshold is not None:
+            resign = v <= self.args.resignThreshold
+        v *= curPlayer # Convert cannonical v into absolute v.
+        self.talk(a, v, resign, verbose=verbose)    
+        return a, v, resign
 
